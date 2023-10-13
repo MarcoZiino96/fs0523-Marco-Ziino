8eda18dc152efb5904fac7ebbc6cfb4bbcb3f7e1
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2023-10-13 17:10:06 +0200

    first commit progetto finale s3 g5

--- mese1/settimana 3/giorno 4/pratica g 4/js esercizio/code.js
+++ mese1/settimana 3/giorno 4/pratica g 4/js esercizio/code.js
@@ -1,38 +1,35 @@
 
 
 let tabella = document.querySelector('#tablenumbers');
-let areabutton = document.querySelector('#areabutton');
-// creazione  bottone 
-let bottone = document.createElement('button');
-bottone.innerText = 'Chiama Numero';
-bottone.classList.add('bottone_style');
+let bottone = document.querySelector('.bottone_style');
 
-for (let i = 1; i <= 100; i++) {
 
-// creazione programmatica di tabella di numeri
-    let numero = document.createElement('div');
-        numero.classList.add('number');
-        numero.innerText = i;
-     
-        
-        
-        tabella.appendChild(numero) 
-        areabutton.append(bottone)  
+for (let i = 1; i <= 90; i++) {
+    let numbertable = document.createElement('div')
+    numbertable.innerText = i;
+    numbertable.classList.add('number');
+    tabella.append(numbertable);    
 }
 
-// creazione del numero randomico al click del bottone
+let areanumber = document.querySelector('#areanumber');
 
-   
-numero_random = [];
-        
-numero_random.push(Math.floor(Math.random()*100))
-
-console.log(numero_random);
-bottone.addEventListener('click', ()=>{
-if (numero_random == numero) {
-    numero.style.backgroundColor = 'red';   
-}    
+bottone.addEventListener('click', () =>{
+  let numberestratto = Math.floor(Math.random()* 90) +1;
+  areanumber.innerText = numberestratto;
+  tabella.children[numberestratto].style.backgroundColor = 'yellow';
 })
+
+
+
+
+    
+    
+        
+        
+    
+    
+    
+    
 
 
 
