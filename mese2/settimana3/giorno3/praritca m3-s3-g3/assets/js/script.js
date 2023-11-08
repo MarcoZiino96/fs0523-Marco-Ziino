0bb2d2a8a9f218f560ec30ed5e1a1a3fd1d4e742
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2023-11-09 00:29:10 +0100

    add metodo con templete delete spaghetti code

--- mese2/settimana3/giorno3/praritca m3-s3-g3/assets/js/script.js
+++ mese2/settimana3/giorno3/praritca m3-s3-g3/assets/js/script.js
@@ -1,37 +1,66 @@
-
-fetch('https://striveschool-api.herokuapp.com/books')
+    
+    let storageCart = localStorage.getItem('cart');
+    let cartArray = storageCart ? JSON.parse(storageCart):[];
+    
+    fetch('https://striveschool-api.herokuapp.com/books')
     .then(res => res.json())
     .then(libri => {
         let containerBooks = document.querySelector('.row');
-
         libri.forEach(libro => {
 
-            let col = document.createElement('div');
-            col.classList.add('col-12', 'col-sm-6', 'col-lg-4', 'col-xl-3' ,'mb-3');
-            containerBooks.append(col);
-            let card = document.createElement('div');
-            card.classList.add('card');
-            col.append(card);
-            let bookImg = document.createElement('img');
-            bookImg.classList.add('card-img-top');
-            let bodyCard = document.createElement('div');
-            bodyCard.classList.add('card-body');
-            card.append(bookImg, bodyCard);
-            bookImg.src = libro.img;
-            let titleBody = document.createElement('h5');
-            titleBody.classList.add('card-title');
-            titleBody.innerText = libro.title;
-            let priceBook = document.createElement('p');
-            priceBook.classList.add('card-text');
-            priceBook.innerText = `${libro.price}$`;
-            let buttonScarta = document.createElement('button');
-                buttonScarta.classList.add('btn' , 'btn-primary');
-                buttonScarta.innerText = 'Scarta';
-                bodyCard.append(titleBody, priceBook, buttonScarta);
+            let cloneCard = getCloneCard();
+
+            let imgCard = cloneCard.querySelector('.card-img-top');
+            let titleCard = cloneCard.querySelector('.card-title');
+            let priceCard = cloneCard.querySelector('.card-text');
+            let buttonScarta = cloneCard.querySelector('.btn.btn-danger');
+            let buttonAdd = cloneCard.querySelector('.btn.btn-primary');
+
+                imgCard.src = libro.img;
+                titleCard.innerText = libro.title;
+                priceCard.innerText = `${libro.price} $ `;
 
                 buttonScarta.addEventListener('click', () => {
-                    col.remove();
+                 let areaCard = document.querySelector('.col-12');
+                     areaCard.remove();
+                     localStorage.removeItem();
                 })
-        })
+                
+                buttonAdd.addEventListener('click', () => {
+                   cartArray.push(libro);
+                   localStorage.setItem('cart', JSON.stringify(cartArray));
+                })            
+                containerBooks.append(cloneCard);
+                
+        });
 
+        
     })
+    function getCloneCard(){
+          let temp = document.getElementsByTagName('template')[0];
+           return     temp.content.cloneNode(true);
+}     
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
