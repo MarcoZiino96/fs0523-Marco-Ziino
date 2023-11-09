f39a371655f2bc16693684543b3ab9614498a051
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2023-11-09 16:58:31 +0100

    load img e img2 e hide img

---
+++ mese2/settimana3/giorno 4/pratica m2-s3-g4/assets/js/script.js
@@ -0,0 +1,59 @@
+
+let query1 = 'https://api.pexels.com/v1/search?query=sunset';
+let query2 = 'https://api.pexels.com/v1/search?query=space';
+let apiKey = '45yUF4ICbVXvWXicRuoehhdaFyRd7jwJJGrMUevDPWwinW9MvZD3pkPP';
+ let btnloadimg = document.querySelector('.btn.btn-primary');
+ let btnloadimgsecondary = document.querySelector('.btn.btn-secondary');
+ let btnHide = document.querySelectorAll('.delete');
+
+
+async function loadImg() {
+    let resp = await fetch(query1, {
+        headers :{
+            'authorization': `${apiKey}`
+        }
+    } );
+
+    let data = await resp.json();
+
+    let imgCard = document.querySelectorAll('.card-img');
+     
+    imgCard.forEach( (img, i) => {
+        img.src = data.photos[i].src.original;
+    });
+
+}
+
+async function loadImg2() {
+    let resp = await fetch(query2, {
+        headers :{
+            'authorization': `${apiKey}`
+        }
+    } );
+
+    let data = await resp.json();
+
+    let imgCard = document.querySelectorAll('.card-img');
+     
+    imgCard.forEach( (img, i) => {
+        img.src = data.photos[i].src.original;
+    });
+
+}
+
+
+
+btnloadimg.addEventListener('click', loadImg);
+btnloadimgsecondary.addEventListener('click', loadImg2);
+
+function deleteimg(card){
+    card.remove();
+}
+
+ btnHide.forEach( btn => {
+    btn.addEventListener('click', ()=> {
+        let areaCard = btn.parentElement.parentElement.parentElement.parentElement;
+        deleteimg(areaCard);
+    });
+ })
+
