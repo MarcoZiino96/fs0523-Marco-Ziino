361e9e97e66ad18c9e67891578a85df39cc13539
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2023-11-11 17:10:50 +0100

    lavoro su modifica per salvare i parametri su modi

--- mese2/settimana3/giorno 5/progetto-settimanale-m2-s3-g5/assets/js/backoffice.js
+++ mese2/settimana3/giorno 5/progetto-settimanale-m2-s3-g5/assets/js/backoffice.js
@@ -4,110 +4,97 @@ let btnCrea = document.querySelector('#btnCrea');
 let btnModifica = document.querySelector('#modifica');
 
 
+
+
 btnCrea.addEventListener('click', async () => {
     let nameProduct = document.querySelector('#brandProduct').value;
     let description = document.querySelector('#description').value;
     let brandProduct = document.querySelector('#brandProduct').value;
     let imgProduct = document.querySelector('#imgProduct').value;
     let priceProduct = document.querySelector('#priceProduct').value;
-    
+
     let newProduct = {
         name: nameProduct,
         description: description,
         brand: brandProduct,
         imageUrl: imgProduct,
         price: priceProduct
-    } 
-    
-     await productCard();
-   async function productCard() {
+    }
+
+    await productCard();
+    async function productCard() {
         let resp = await fetch(fetchUrl, {
-        method: 'POST',
-        headers: {
-            Authorization: apiKey,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newProduct)
-     })
-     
-     if (resp.status === 200) {
-  
+            method: 'POST',
+            headers: {
+                Authorization: apiKey,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newProduct)
+        })
+
+        if (resp.status === 200) {
+
+
             Swal.fire({
                 position: "top-end",
                 icon: "success",
                 title: "prodotto creato con successo!",
                 showConfirmButton: false,
                 timer: 1500
-              });
+            });
 
-              
-    
-
-        }else{
+            setTimeout(() => {
+                location.href = "../crud/homepage.html";
+            }, 2000);
+        } else {
             Swal.fire({
                 position: "top-end",
                 icon: "error",
-                title: "prodotto non creato!",
+                title: "prodotto non creato! Riempi almeno un campo!",
                 showConfirmButton: false,
-                timer: 1500
+                timer: 1800
             });
-                location.href = '../crud/homepage.html';    
-     }
-     
-}  
-
+        }
+    }
 });
 
-btnModifica.addEventListener('click', async () => {
+let url = new URLSearchParams(location.search);
+let id = url.get('id')
+console.log(id);
 
+btnModifica.addEventListener('click', async () => {
+ 
     let nameProduct = document.querySelector('#brandProduct').value;
     let description = document.querySelector('#description').value;
     let brandProduct = document.querySelector('#brandProduct').value;
     let imgProduct = document.querySelector('#imgProduct').value;
     let priceProduct = document.querySelector('#priceProduct').value;
-    
+
     let newProduct = {
         name: nameProduct,
         description: description,
         brand: brandProduct,
         imageUrl: imgProduct,
         price: priceProduct
-    } 
+    }
 
-   
-    let url = new URLSearchParams(location.search);
-    let id = url.get('id');
-
- await modificaCard(id);
-async function modificaCard(id){
-  let resp = await fetch(fetchUrl + id , {
-    method: 'PUT',
-    headers: {
-        Authorization: apiKey,
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(newProduct)
-})
-  if (resp.status === 200) {
-     
     
-        Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "prodotto modificato con successo!",
-            showConfirmButton: false,
-        });
-       
-     } else{
-        Swal.fire({
-            position: "top-end",
-            icon: "error",
-            title: "Prodotto non modificato! Inserisci almeno un campo!",
-            showConfirmButton: false,
-            timer: 1500
-        });
-     }
-}  
+    
+    let url = new URLSearchParams(location.search);
+    let id = url.get('id')
+    await modificaCard(id);
+    async function modificaCard(id) {
+        let resp = await fetch(fetchUrl + id, {
+            method: 'PUT',
+            headers: {
+                Authorization: apiKey,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newProduct)
+        })
+
+      
+    }
 
 })
 
