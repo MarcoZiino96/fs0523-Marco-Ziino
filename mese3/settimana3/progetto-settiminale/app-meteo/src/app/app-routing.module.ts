e13e1d13e08bfb16aaace34debad88aef51ccb50
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2023-12-07 19:24:00 +0100

    setting app-meteo

---
+++ mese3/settimana3/progetto-settiminale/app-meteo/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: '/home',
+    pathMatch: 'full'
+
+  },
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
