7c17283202929300f3dc6871c853d48bd627e1e7
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2023-12-07 20:25:08 +0100

    creazione model con interfece e environments

---
+++ mese3/settimana3/progetto-settiminale/app-meteo/src/app/pages/Models/i-user.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  id:string;
+  nome:string;
+  cognome:string;
+  email:string;
+  password:string;
+}
--- mese3/settimana3/progetto-settiminale/app-meteo/src/app/pages/auth/auth.service.ts
+++ mese3/settimana3/progetto-settiminale/app-meteo/src/app/pages/auth/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { IAccessData } from '../Models/i-access-data';
+import { IRegister } from '../Models/i-register';
+import { Observable } from 'rxjs';
+import { ILogin } from '../Models/i-login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  registerUrl:string = environment.apiUrl + `/register`
+  loginUrl:string = environment.apiUrl + `/login`
+
+
+  singUp(data:IRegister):Observable<IAccessData>{
+   return this.http.post<IAccessData>(this.registerUrl, data)
+  }
+
+  login(data:ILogin):Observable<IAccessData>{
+    return this.http.post<IAccessData>(this.loginUrl, data)
+   }
+
 }
